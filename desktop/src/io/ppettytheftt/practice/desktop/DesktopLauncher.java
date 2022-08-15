e6cbb4e1d74fa21af6e29bc45568a7f717e99ec8
ppettytheftt <jason.c.coffman23@gmail.com>
2022-08-15 12:34:25 -0500

    pushing game code

---
+++ desktop/src/io/ppettytheftt/practice/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.ppettytheftt.practice.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.ppettytheftt.practice.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
