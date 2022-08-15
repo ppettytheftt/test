d1fc6e71b2116f7b63af718b19b14f35cb72994a
ppettytheftt <jason.c.coffman23@gmail.com>
2022-08-15 14:46:22 -0500

    adds skin.json to assets and need to get the play screen going

--- desktop/src/io/ppettytheftt/practice/desktop/DesktopLauncher.java
+++ desktop/src/io/ppettytheftt/practice/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import io.ppettytheftt.practice.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Work in Progress";
+		config.useGL30 = false;
+		config.width = 480;
+		config.height = 320;
+
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
