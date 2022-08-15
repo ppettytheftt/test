dab3d526472662bbd5f4fb1d588c9c85a3376fd1
ppettytheftt <jason.c.coffman23@gmail.com>
2022-08-15 13:09:23 -0500

    adds the other methods from the interface in the Mygame class

--- core/src/io/ppettytheftt/practice/MyGdxGame.java
+++ core/src/io/ppettytheftt/practice/MyGdxGame.java
@@ -6,26 +6,41 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    SpriteBatch batch;
+    Texture img;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("PlayerRoom1.png");
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(1, 0, 0, 1);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
 }
